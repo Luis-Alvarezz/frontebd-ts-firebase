1c5953165b32f3120a0c01c29dbbff9107be27c9
Luis-Alvarezz <luisangelug10@gmail.com>
2025-02-11 10:13:06 -0600

    [CREATE]: Creacion de compiente para mostrar las alertas

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-  </div>
+  <v-app>
+    <v-container fill-height>
+      <v-row justify="center" align="center" class="fil-height">
+        <v-col cols="12" sm="8" md="4">
+          <v-alert
+            v-if="$store.state.alert.show"
+            :type="$store.state.alert.type"
+            dismissible
+          >
+            {{ $store.state.alert.message }}
+          </v-alert>
+          <Nuxt />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
