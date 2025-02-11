f34ab86f011bc5626b54639d4809d04f0c3abb6c
Luis-Alvarezz <luisangelug10@gmail.com>
2025-02-11 10:14:46 -0600

    [CREATE]: Se creo vista para el login, enlazando logica con backend

---
+++ components/auth/userLogin.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Iniciar Sesión
+    </v-card-title>
+
+    <v-card-text>
+      <v-text-field
+        v-model="username"
+        label="Usuario"
+        outlined
+      />
+
+      <v-text-field
+        v-model="password"
+        label="Password"
+        outlined
+        type="password"
+      />
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="primary" @click="login">
+        Ingresar
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        // ! Loggeo, enlazando con el backend
+        await this.$auth.loginWith('local', {
+          data: {
+            usuario: this.username,
+            password: this.password
+          }
+        })
+        this.$store.dispatch('alert/showAlert', {
+          message: 'Inicio de Sesión Correcto',
+          type: 'success'
+        })
+        this.$router.push('/dashboard')
+      } catch (error) {
+        this.$store.dispatch('alert/showAlert', {
+          message: error.message,
+          type: 'error'
+        })
+      }
+    }
+  }
+}
+</script>
